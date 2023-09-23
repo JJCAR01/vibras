f71cc6e343a099b10fa15fe43f09e10edcd5286e
Dalejam <dalisalmova@gmail.com>
2023-09-23 10:57:55 -0500

    agregar tipo documento en usuario

--- vibras/src/main/java/com/vibras/dominio/modelo/Usuario.java
+++ vibras/src/main/java/com/vibras/dominio/modelo/Usuario.java
@@ -12,26 +12,32 @@ import lombok.Setter;
 public class Usuario {
     private final String nombre;
     private final String apellido;
+    private  int tipoDocumento;
+    private  String numeroDocumento;
     private final String correo;
     private String contrasena;
-    private int tipoUsuario;
+    private String rol;
     private String celular;
 
-    public static Usuario of(String nombre, String apellido, String correo, String contrasena, int roles,String celular) {
+    public static Usuario of(String nombre, String apellido,int tipoDocumento,String numeroDocumento, String correo, String contrasena, String rol,String celular) {
         UtilTexto.validarObligatorio(nombre,"El nombre no puede ser vacio");
         UtilTexto.validarObligatorio(apellido,"El apellido no puede ser vacio");
+        UtilNumero.numeroNoPuedeSerNegativo(tipoDocumento,"El tipo de documento no puede ser negativo");
+        UtilTexto.validarObligatorio(numeroDocumento,"El numero de documento no puede ser negativo");
         UtilTexto.validarObligatorio(correo,"El correo no puede ser vacio");
         UtilTexto.validarObligatorio(contrasena,"La contraseña no puede ser vacia");
         UtilTexto.validarLongitudContrasena(contrasena,"La contraseña debe tener minimo un numero");
-        UtilNumero.numeroDiferenteCero(roles,"Rol debe ser diligenciado");
-        return  new Usuario(nombre,apellido,correo,contrasena,roles,celular);
+        UtilTexto.validarObligatorio(rol,"Rol debe ser diligenciado");
+        return  new Usuario(nombre,apellido,tipoDocumento,numeroDocumento,correo,contrasena,rol,celular);
     }
-    private Usuario(String nombre, String apellido, String correo, String contrasena, int roles,String celular) {
+    private Usuario(String nombre, String apellido,int tipoDocumento,String numeroDocumento, String correo, String contrasena, String roles,String celular) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDocumento = numeroDocumento;
         this.correo = correo;
         this.contrasena = contrasena;
-        this.tipoUsuario = roles;
+        this.rol = rol;
         this.celular = celular;
     }
 
